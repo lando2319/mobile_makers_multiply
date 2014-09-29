469eb3a7b77bb462245e1509d6ba89bd1697791e
Mike Land <mikepland@gmail.com>
2014-09-29 16:09:49 -0500

    Initial Commit

---
+++ Multiply/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multiply
+//
+//  Created by MIKE LAND on 9/29/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Multiply/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Multiply
+//
+//  Created by MIKE LAND on 9/29/14.
+//  Copyright (c) 2014 MIKE LAND. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
